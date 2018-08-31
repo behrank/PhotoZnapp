b3a94ed6854dfd947e4ab94fd9f88b15993236f2
Behran Kankul <behrank@gmail.com>
2018-08-31 14:50:03 +0300

    Initial Commit

---
+++ PhotoZnapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoZnapp
+//
+//  Created by Behran Kankul on 31.08.2018.
+//  Copyright © 2018 Be Mobile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
